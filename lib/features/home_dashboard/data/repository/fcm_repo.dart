c5e56c91a98f8a94ac3b89403c947f211e0a5e37
eliusa0997 <eliusaomer@gmail.com>
2026-03-24 07:00:10 +0200

    fcm working

---
+++ lib/features/home_dashboard/data/repository/fcm_repo.dart
@@ -0,0 +1,22 @@
+import 'package:eps_pay/core/networking/api_result.dart';
+import 'package:eps_pay/core/networking/api_service.dart';
+import 'package:eps_pay/features/auth/login/data/model/fcm_request_body.dart';
+import 'package:eps_pay/features/auth/login/data/model/fcm_response.dart';
+import 'package:eps_pay/features/home_dashboard/data/model/profile_model.dart';
+
+class FcmRepo {
+  final ApiService _apiService;
+  FcmRepo(this._apiService);
+
+  Future<ApiResult<FcmResponse>> sendFcmTokenToServer(
+    FcmRequestBody fcmRequestBody,
+  ) async {
+    try {
+      final response = await _apiService.sendFcmToken(fcmRequestBody);
+
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(error.toString());
+    }
+  }
+}
