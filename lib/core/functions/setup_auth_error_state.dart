64dffb8c90c3683ba05d350400bf99e7368288ea
eliusa0997 <eliusaomer@gmail.com>
2026-04-04 22:16:59 +0200

    refactoring error handler

--- lib/core/functions/setup_auth_error_state.dart
+++ lib/core/functions/setup_auth_error_state.dart
@@ -1,15 +1,16 @@
 import 'package:eps_pay/core/helpers/extensions.dart';
+import 'package:eps_pay/core/networking/api_error_model.dart';
 import 'package:eps_pay/core/theming/colors.dart';
 import 'package:flutter/material.dart';
 
-void setupErrorState(BuildContext context, String error) {
+void setupErrorState(BuildContext context, ApiErrorModel apiErrorModel) {
   context.pop();
   showDialog(
     context: context,
     builder: (context) {
       return AlertDialog(
         icon: const Icon(Icons.error, color: AppColors.error, size: 32),
-        content: Text(error),
+        content: Text(apiErrorModel.detail.toString()),
         actions: [
           TextButton(
             onPressed: () {
