f5b5c7bf57a4b24ef9a6d933861766eb55e97ef2
eliusa0997 <eliusaomer@gmail.com>
2026-08-21 20:56:44 +0200

    change the server

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -9,7 +9,7 @@ class DioFactory {
   static Dio? dio;
   static final FlutterSecureStorage _storage = FlutterSecureStorage();
   static Dio getDio() {
-    Duration timeOut = const Duration(seconds: 30);
+    Duration timeOut = const Duration(seconds: 100);
 
     if (dio == null) {
       dio = Dio();
